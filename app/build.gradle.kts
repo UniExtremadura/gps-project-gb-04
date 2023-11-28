a4d229de4ef71f0762fbd59608971808606a5b64
Seslavav <seslavav@alumnos.unex.es>
2023-11-28 23:08:34 +0100

    Dependencies configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
     namespace = "es.unex.giss.asee.ghiblitrunk"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "es.unex.giss.asee.ghiblitrunk"
@@ -15,6 +17,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
     }
 
     buildTypes {
@@ -30,8 +33,17 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    viewBinding{
+        enable = true
+    }
+    dataBinding{
+        enable = true
+    }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures{
+        buildConfig = true
     }
 }
 
@@ -44,4 +56,59 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //////////////////////////////////////////
+    ////// ROOM                 //////////////
+    //////////////////////////////////////////
+
+    val room_version = "2.5.0"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // To use Kotlin Symbol Processing (KSP)
+    ksp("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
+
+    //////////////////////////////////////////
+    ////// NAVIGATION COMPONENT //////////////
+    //////////////////////////////////////////
+
+    val nav_version = "2.5.3"
+
+    // Kotlin implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // Feature module Support
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+
+    // Testing Navigation
+    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
+
+    // Jetpack Compose Integration
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    ////////////////////////////////////
+    /////////// API ////////////////////
+    ////////////////////////////////////
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    ////////////////////////////////////
+    /////////// RECYCLERVIEW ///////////
+    ////////////////////////////////////
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+
+    ////////////////////////////////////
+    ////////////// PICASSO /////////////
+    ////////////////////////////////////
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
+
 }
