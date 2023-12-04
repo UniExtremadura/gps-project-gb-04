789a411aaf194dac6da71fc7bf59ee90af9cb963
SergioTrac <sergiotraceslava@gmail.com>
2023-12-04 23:38:24 +0100

    GB04-16 Funcionalidad de inicio de sesión

---
+++ app/src/main/java/es/unex/giss/asee/ghiblitrunk/login/UserManager.kt
@@ -0,0 +1,43 @@
+package es.unex.giss.asee.ghiblitrunk.login
+
+import android.content.Context
+import es.unex.giss.asee.ghiblitrunk.data.models.User
+import es.unex.giss.asee.ghiblitrunk.database.GhibliTrunkDatabase
+
+object UserManager {
+
+    fun saveCurrentUser(context: Context, user: User?) {
+        val sharedPrefs = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPrefs.edit()
+
+        if (user != null) {
+            // Guardar detalles del usuario
+            editor.putString("username", user.name)
+        } else {
+            // Limpiar detalles del usuario si es nulo
+            editor.remove("username")
+        }
+
+        editor.apply()
+    }
+
+    suspend fun loadCurrentUser(context: Context) : User? {
+        val sharedPrefs = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+        val username = sharedPrefs.getString("username", null)
+        if (username != null) {
+            // Obtener instancia de la base de datos
+            val database = GhibliTrunkDatabase.getInstance(context)
+
+            // Usar el DAO para buscar al usuario
+            return database.userDao().find(username)
+        }
+        return null
+    }
+
+    suspend fun clearData(context: Context){
+        val sharedPrefs = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPrefs.edit()
+
+        editor.remove("username")
+    }
+}
---
+++ app/src/main/java/es/unex/giss/asee/ghiblitrunk/view/WelcomeActivity.kt
@@ -0,0 +1,56 @@
+package es.unex.giss.asee.ghiblitrunk.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import es.unex.giss.asee.ghiblitrunk.data.models.User
+import es.unex.giss.asee.ghiblitrunk.databinding.ActivityWelcomeBinding
+import es.unex.giss.asee.ghiblitrunk.login.UserManager
+import es.unex.giss.asee.ghiblitrunk.view.home.HomeActivity
+import kotlinx.coroutines.launch
+
+class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
+    companion object {
+        val LOGIN_USER = "LOGIN_USER"
+        const val DELAY_MILLIS = 2000L // Puedes ajustar el tiempo de retraso según sea necesario
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Restaurar el usuario actual desde SharedPreferences
+        lifecycleScope.launch {
+            var currentUser: User? = UserManager.loadCurrentUser(applicationContext)
+
+            if (currentUser == null) {
+                // No hay un usuario actual, ir a LoginActivity
+                startActivity(Intent(applicationContext, LoginActivity::class.java))
+                finish() // Cerrar WelcomeActivity si está abierta
+            } else {
+                // Hay un usuario actual, mostrar mensaje de bienvenida y cerrar WelcomeActivity después del retraso
+                binding.textView2.text = "Welcome back, ${currentUser.name}"
+
+                Handler(Looper.getMainLooper()).postDelayed({
+                    finishWelcomeActivity()
+                }, DELAY_MILLIS)
+            }
+        }
+    }
+
+    private fun finishWelcomeActivity() {
+        val welcomeActivity = this@WelcomeActivity
+        welcomeActivity.runOnUiThread {
+            welcomeActivity.finish()
+            // Iniciar HomeActivity
+            startActivity(Intent(welcomeActivity, HomeActivity::class.java))
+        }
+    }
+}
