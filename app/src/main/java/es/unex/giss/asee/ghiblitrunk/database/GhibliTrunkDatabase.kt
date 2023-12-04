5735e3adcba68136b1f2c5a2730d32ee17640513
lee_chong <jchongoj@alumnos.unex.es>
2023-12-05 00:18:10 +0100

    GB04-38 Conexión de los datos recibidos con la base de datos local

--- app/src/main/java/es/unex/giss/asee/ghiblitrunk/database/GhibliTrunkDatabase.kt
+++ app/src/main/java/es/unex/giss/asee/ghiblitrunk/database/GhibliTrunkDatabase.kt
@@ -4,11 +4,18 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import es.unex.giss.asee.ghiblitrunk.data.models.Character
+import es.unex.giss.asee.ghiblitrunk.data.models.Movie
 import es.unex.giss.asee.ghiblitrunk.data.models.User
+import es.unex.giss.asee.ghiblitrunk.data.models.Converters
 
-@Database(entities = [User::class], version = 1, exportSchema = false)
+@Database(entities = [User::class, Movie::class, Character::class], version = 2, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class GhibliTrunkDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
+    abstract fun movieDao(): MovieDao
+    abstract fun characterDao(): CharacterDao
 
     companion object {
         private var INSTANCE: GhibliTrunkDatabase? = null
