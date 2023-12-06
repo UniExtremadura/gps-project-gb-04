ff5de6f3be0421d36e9466468681d9d57db55d68
Seslavav <seslavav@alumnos.unex.es>
2023-12-06 03:08:04 +0100

    Añadida funcionalidad de relacionar usuario con sus personajes favoritos. Actualizada base de datos

--- app/src/main/java/es/unex/giss/asee/ghiblitrunk/database/CharacterDao.kt
+++ app/src/main/java/es/unex/giss/asee/ghiblitrunk/database/CharacterDao.kt
@@ -5,7 +5,14 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
 import es.unex.giss.asee.ghiblitrunk.data.models.Character
+import es.unex.giss.asee.ghiblitrunk.data.models.Movie
+import es.unex.giss.asee.ghiblitrunk.data.models.UserCharacterCrossRef
+import es.unex.giss.asee.ghiblitrunk.data.models.UserMovieCrossRef
+import es.unex.giss.asee.ghiblitrunk.data.models.UserWithCharacters
+import es.unex.giss.asee.ghiblitrunk.data.models.UserWithMovies
 
 @Dao
 interface CharacterDao {
@@ -15,6 +22,9 @@ interface CharacterDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertAll(characters: List<Character>)
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertUserCharacter(crossRef: UserCharacterCrossRef)
+
     @Query("SELECT count(*) FROM characters")
     suspend fun getNumberOfCharacters(): Long
 
@@ -23,4 +33,11 @@ interface CharacterDao {
 
     @Query("SELECT * FROM characters WHERE id = :id")
     suspend fun getPersonById(id: String): Character
+
+    @Update
+    suspend fun update(character: Character)
+
+    @Transaction
+    @Query("SELECT * FROM User where userId = :userId")
+    fun getUserWithCharacters(userId: Long): LiveData<UserWithCharacters>
 }
